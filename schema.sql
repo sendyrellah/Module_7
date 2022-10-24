aa2b322931ed131c2f406bbdfd8ff780522c3eb0
Sendy Nguyen <sendy.nguyen@icloud.com>
2022-10-23 21:32:01 -0500

    pushing new quieries for SQL

--- schema.sql
+++ schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE departments (
 );
 
 CREATE TABLE employees (
-	emp_no DATE NOT NULL,
+	emp_no INT NOT NULL,
 	birth_date VARCHAR NOT NULL,
 	first_name VARCHAR NOT NULL,
 	last_name VARCHAR NOT NULL,
@@ -30,20 +30,20 @@ CREATE TABLE managers (
 	dept_no VARCHAR NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
-	to_date DATE NOT NULL
+	to_date DATE NOT NULL,
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 PRIMARY KEY (dept_no, emp_no)
 );
 
 CREATE TABLE dept_emp (
-	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-PRIMARY KEY (dept_no, emp_no),
+PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE title (
@@ -51,8 +51,7 @@ CREATE TABLE title (
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-PRIMARY KEY (emp_no)
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 SELECT * FROM departments;
